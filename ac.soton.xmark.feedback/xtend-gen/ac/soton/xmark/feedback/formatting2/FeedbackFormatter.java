971b5509d7e7ebaeaa5922e3e815ef47fc0966c2
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2018-06-09 23:38:36 +0100

    Regenerated

--- ac.soton.xmark.feedback/xtend-gen/ac/soton/xmark/feedback/formatting2/FeedbackFormatter.java
+++ ac.soton.xmark.feedback/xtend-gen/ac/soton/xmark/feedback/formatting2/FeedbackFormatter.java
@@ -12,8 +12,10 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.formatting2.AbstractFormatter2;
 import org.eclipse.xtext.formatting2.IFormattableDocument;
+import org.eclipse.xtext.formatting2.IHiddenRegionFormatter;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 @SuppressWarnings("all")
 public class FeedbackFormatter extends AbstractFormatter2 {
@@ -22,10 +24,50 @@ public class FeedbackFormatter extends AbstractFormatter2 {
   private FeedbackGrammarAccess _feedbackGrammarAccess;
   
   protected void _format(final ExerciseFeedback exerciseFeedback, @Extension final IFormattableDocument document) {
-    EList<Feedback> _feedbacks = exerciseFeedback.getFeedbacks();
-    for (final Feedback feedback : _feedbacks) {
-      document.<Feedback>format(feedback);
+    final EList<Feedback> feedbacks = exerciseFeedback.getFeedbacks();
+    for (final Feedback feedback : feedbacks) {
+      {
+        final Procedure1<IHiddenRegionFormatter> _function = (IHiddenRegionFormatter it) -> {
+          it.setNewLines(2);
+        };
+        document.<Feedback>prepend(document.<Feedback>format(feedback), _function);
+        final Procedure1<IHiddenRegionFormatter> _function_1 = (IHiddenRegionFormatter it) -> {
+          it.indent();
+        };
+        document.<Feedback>interior(feedback, _function_1);
+      }
     }
+    final Procedure1<IHiddenRegionFormatter> _function = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.append(this.textRegionExtensions.allRegionsFor(exerciseFeedback).ruleCallTo(this._feedbackGrammarAccess.getML_COMMENTRule()), _function);
+    final Procedure1<IHiddenRegionFormatter> _function_1 = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.prepend(this.textRegionExtensions.regionFor(exerciseFeedback).keyword("end"), _function_1);
+  }
+  
+  protected void _format(final Feedback feedback, @Extension final IFormattableDocument document) {
+    final Procedure1<IHiddenRegionFormatter> _function = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.prepend(this.textRegionExtensions.regionFor(feedback).keyword("recipients"), _function);
+    final Procedure1<IHiddenRegionFormatter> _function_1 = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.prepend(this.textRegionExtensions.regionFor(feedback).keyword("title"), _function_1);
+    final Procedure1<IHiddenRegionFormatter> _function_2 = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.prepend(this.textRegionExtensions.regionFor(feedback).keyword("feedback"), _function_2);
+    final Procedure1<IHiddenRegionFormatter> _function_3 = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.prepend(this.textRegionExtensions.regionFor(feedback).keyword("}"), _function_3);
+    final Procedure1<IHiddenRegionFormatter> _function_4 = (IHiddenRegionFormatter it) -> {
+      it.newLine();
+    };
+    document.append(this.textRegionExtensions.allRegionsFor(feedback).ruleCallTo(this._feedbackGrammarAccess.getML_COMMENTRule()), _function_4);
   }
   
   public void format(final Object exerciseFeedback, final IFormattableDocument document) {
@@ -34,6 +76,9 @@ public class FeedbackFormatter extends AbstractFormatter2 {
       return;
     } else if (exerciseFeedback instanceof ExerciseFeedback) {
       _format((ExerciseFeedback)exerciseFeedback, document);
+      return;
+    } else if (exerciseFeedback instanceof Feedback) {
+      _format((Feedback)exerciseFeedback, document);
       return;
     } else if (exerciseFeedback instanceof EObject) {
       _format((EObject)exerciseFeedback, document);
